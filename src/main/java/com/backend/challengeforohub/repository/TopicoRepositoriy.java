04be59811a115738535f2277904fb1ca882dd047
CasseliLayza <casseli.layza@gmail.com>
2025-01-21 23:55:24 -0500

    doc: readme

--- src/main/java/com/backend/challengeforohub/repository/TopicoRepositoriy.java
+++ src/main/java/com/backend/challengeforohub/repository/TopicoRepositoriy.java
@@ -1,7 +1,6 @@
 package com.backend.challengeforohub.repository;
 
 import com.backend.challengeforohub.entity.Topico;
-import com.backend.challengeforohub.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
